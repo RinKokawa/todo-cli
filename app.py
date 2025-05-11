921c81c7aa12e8b03d3786035565c324857a9b28
RinKokawa <rinkokawa@outlook.com>
2025-05-11 16:11:46 +0800

    完成任务 37：新增rename方法，todo rename [id] [string] ,可以将[id]的内容改成[string] —— 已初始化rename方法

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from commands.current import current as current_command
 from commands.change import change as change_command
 from commands.import_tapd import import_tapd as import_tapd_command
 
+# app.py 中追加注册rename
+from commands.rename import rename as rename_command
+
 app = typer.Typer(help="📌 一个简单的 CLI Todo 工具，支持嵌套任务与树形结构展示。")
 
 app.command()(list_command)
@@ -19,6 +22,8 @@ app.command()(delete_command)
 app.command()(current_command)
 app.command()(change_command)
 app.command()(import_tapd_command)
+
+app.command()(rename_command)
 
 if __name__ == "__main__":
     app()
